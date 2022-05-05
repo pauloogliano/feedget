e426da02c495e31c396d99a861daae09e2a07991
Paulo Ogliano <contato@pauloogliano.com>
2022-05-05 23:04:32 +0100

    Removing dependencies not use

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,9 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
-import { prisma } from './prisma';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
-
-
 
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
