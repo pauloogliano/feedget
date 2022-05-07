083688958bddf28575d64b81a1f16c24c54aff8f
Paulo Ogliano <contato@pauloogliano.com>
2022-05-07 19:04:31 +0100

    Removed space (default)

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -51,7 +51,7 @@ export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props
     try {
       await api.post('/feedbacks', {
         type: feedbackType,
-        screenshot: `data:image/png;base64, ${screenshotBase64}`,
+        screenshot: `data:image/png;base64,${screenshotBase64}`,
         comment
       });
 
