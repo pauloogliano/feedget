e68053c1217b94904cda8d64d3f761314dac3742
Paulo Ogliano <contato@pauloogliano.com>
2022-05-07 18:42:22 +0100

    Mobile app created and integrated with back-end

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,47 @@
+import 'react-native-gesture-handler';
+// import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 
+// import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 
 export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: theme.colors.background
-    }}>
+  SplashScreen.preventAutoHideAsync();
 
-      <Widget />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    // return <AppLoading />;
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background
+      }}
+    >
 
       <StatusBar
         style="light"
         backgroundColor="transparent"
         translucent
       />
+
+      <Widget />
+
     </View>
   );
 }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import { Options } from '../Options';
+import { Form } from '../Form';
+import { Success } from '../Success';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
 
-export function Widget() {
+export type FeedbackType = keyof typeof feedbackTypes;
+
+function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>();
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true);
+  }
+
   return (
-    <View style={styles.container}>
+    <>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleOpen}
+      >
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
 
-    </View>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        {
+          feedbackSent
+          ?
+          <Success onSendAnotherFeedback={handleRestartFeedback} />
+          :
+          <>
+            {
+              feedbackType
+              ?
+              <Form
+                feedbackType={feedbackType}
+                onFeedbackCanceled={handleRestartFeedback}
+                onFeedbackSent={handleFeedbackSent}
+              />
+              :
+              <Options onFeedbackTypeChanged={setFeedbackType} />
+            }
+          </>
+        }
+      </BottomSheet>
+
+    </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
